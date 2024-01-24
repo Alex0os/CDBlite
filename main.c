05bca57fa8c1df40278a8ed7c5b8125324fcde51
Matías A. <lex0ss2062@gmail.com>
2024-01-23 23:02:09 -0300

    2nd Commit: Adding complexity to the REPL

--- main.c
+++ main.c
@@ -9,6 +9,26 @@ typedef struct {
 	size_t input_length;
 } InputBuffer;
 
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+	STATEMENT_INSERT,
+	STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+} Statement;
+
+
 InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
 	input_buffer->buffer = NULL;
@@ -16,6 +36,47 @@ InputBuffer* new_input_buffer() {
 	input_buffer->input_length = 0;
 
 	return input_buffer;
+}
+
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+	if (strcmp(input_buffer->buffer, ".exit") != 0) {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+	close_input_buffer(input_buffer);
+	exit(EXIT_SUCCESS);
+}
+
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+
+void execute_statement(Statement* statement){
+	switch (statement->type) {
+		case (STATEMENT_INSERT):
+			printf("This is where we would do an insert.\n");
+			break;
+		case (STATEMENT_SELECT):
+			printf("This is where we would do an select.\n");
+			break;
+	}
 }
 
 void read_input(InputBuffer* input_buffer){
@@ -30,11 +91,6 @@ void read_input(InputBuffer* input_buffer){
 	input_buffer->buffer[bytes_read - 1] = '\0';
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
-}
-
 // Creation of the REPL 
 int main(int argc, char *argv[]){
 	InputBuffer* input_buffer = new_input_buffer();
@@ -43,13 +99,25 @@ int main(int argc, char *argv[]){
 		printf("db > ");
 		read_input(input_buffer);
 
-		if (strcmp(input_buffer->buffer, "exit") == 0) {
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
-		} else {
-			printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		if (input_buffer->buffer[0] == '.') {
+			switch (do_meta_command(input_buffer)) {
+				case (META_COMMAND_SUCCESS):
+					continue;
+				case (META_COMMAND_UNRECOGNIZED_COMMAND):
+					printf("Unrecognized command\n");
+					continue;
+			}
 		}
+		Statement statement;
+		switch (prepare_statement(input_buffer, &statement)) {
+			case (PREPARE_SUCCESS):
+				break;
+			case (PREPARE_UNRECOGNIZED_STATEMENT):
+				printf("Unrecognized keyword at the start %s\n", input_buffer->buffer);
+				continue;
+		}
+		execute_statement(&statement);
+		printf("Executed\n");
 	}
-
 	return 0;
 }
