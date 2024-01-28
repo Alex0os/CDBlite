f40b15af9d5e4c32b0625ab8a476a60d907147ab
Matías A. <lex0ss2062@gmail.com>
2024-01-28 13:45:24 -0300

    4th Commit: Updating documentation. First steps to interact with database (insert and select)

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+#define size_of_member(Struct, Member) sizeof(((Struct*)0)->Member)
+
+#define PAGE_SIZE 4096
+#define TABLE_MAX_PAGES 100
+
+
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE];
+	char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 typedef struct {
 	char* buffer;
@@ -16,7 +32,8 @@ typedef enum {
 
 typedef enum {
 	PREPARE_SUCCESS,
-	PREPARE_UNRECOGNIZED_STATEMENT
+	PREPARE_UNRECOGNIZED_STATEMENT,
+	PREPARE_SYNTAX_ERROR
 } PrepareResult;
 
 typedef enum {
@@ -26,8 +43,39 @@ typedef enum {
 
 typedef struct {
 	StatementType type;
+	Row row_to_insert;
 } Statement;
 
+typedef struct {
+	uint32_t num_rows;
+	void* pages[TABLE_MAX_PAGES];
+} Table;
+
+
+const uint32_t ID_SIZE = size_of_member(Row, id);
+const uint32_t USERNAME_SIZE = size_of_member(Row, username);
+const uint32_t EMAIL_SIZE = size_of_member(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROW = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+// In this point we need to define the compact verision of a row to store them into pages inside memory
+
+void serialize_row(Row* source, void* destination){
+	memcpy(source + ID_OFFSET, &(source->id), ID_SIZE);
+	memcpy(source + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+	memcpy(source + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* destination ,Row* source){
+	memcpy(&(source->id), source + ID_OFFSET, ID_SIZE);
+	memcpy(&(source->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+	memcpy(&(source->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
 
 InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -57,6 +105,16 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
 	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
 		statement->type = STATEMENT_INSERT;
+
+		// In this section we'll parse the arguments obtained by the prompt
+		int arguments_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
+				&(statement->row_to_insert.id),
+				&(statement->row_to_insert.username),
+				&(statement->row_to_insert.email));
+		if (arguments_assigned < 3) {
+			return PREPARE_SYNTAX_ERROR;
+		}
+
 		return PREPARE_SUCCESS;
 	}
 	if (strcmp(input_buffer->buffer, "select") == 0) {
